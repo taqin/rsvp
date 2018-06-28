c7c5fb4120f95d03f9c32bd5ea30d4f5e007f83a
Taqin <taqin@megentics.com>
2018-06-28 16:08:34 +0800

    Shift to env variables

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
-require('./config/config');
+const dotenv = require('dotenv');
+const result = dotenv.config();
+
+if (result.error) {
+  throw result.error;
+}
 
 const _ = require('lodash');
 
--- config/config.js
+++
@@ -1,7 +0,0 @@
-const env = process.env.NODE_ENV || 'development';
-
-if (env === 'development') {
-  process.env.PROXY = 'http://';
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/rsvp';
-} 
